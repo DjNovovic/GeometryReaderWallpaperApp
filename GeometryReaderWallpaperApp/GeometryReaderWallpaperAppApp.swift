bdddd5f9127662fb59764f6bbf1d5336a5fc46e5
DjNovovic <djordjijenovovic@gmail.com>
2021-09-06 17:52:15 +0200

    Initial Commit

---
+++ GeometryReaderWallpaperApp/GeometryReaderWallpaperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderWallpaperAppApp.swift
+//  GeometryReaderWallpaperApp
+//
+//  Created by Đorđije Novović on 6.9.21..
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderWallpaperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
